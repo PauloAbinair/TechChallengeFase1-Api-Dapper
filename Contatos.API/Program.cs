e296f9ef9407f8574271ba2e7d28ddf63cf0835f
Paulo Silva <paulo.silva@faciltech.com.br>
2024-06-28 00:09:53 -0300

    feature: testes de integração

--- Contatos.API/Program.cs
+++ Contatos.API/Program.cs
@@ -109,3 +109,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Make the implicit Program class public so test projects can access it
+public partial class Program { }
---
+++ Contatos.Integration.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override IHostBuilder CreateHostBuilder()
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<TStartup>();
+            });
+    }
+}
